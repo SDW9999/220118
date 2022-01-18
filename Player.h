1ebc5171e4f19bdda2970cc2e2bc1e7fb94c2e9a
SDW9999 <ted129@naver.com>
2022-01-18 13:55:13 +0900

    accessor 추가 const 추가 접근지정자 설정

--- Player.h
+++ Player.h
@@ -1,13 +1,27 @@
 #pragma once
 
+// private - 캡슐화
+
 class Player // 데이터 타입
 {
-public:
+private:
 	int HP;
 	int MP;
 	int Gold;
 
 public:
+	// 접근자 (accessor)
+	void SetHP(int NewHP)
+	{
+		if (NewHP >= 0)
+		{
+			HP = NewHP;
+		}
+	}
+	int GetHP() const
+	{
+		return HP;
+	}
 	void Collect();
 	void Move();
 	void Attack();
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	Player NewPlayer; // 메모리에 생성되면 인스턴스(오브젝트)라고 함
 	
+	NewPlayer.SetHP(-100); // -100으로 넣어서 무결성
 
 
 	return 0;
